5dd3ff92922c1b31a73f9238070ed79269d400f4
Denis Eroshevskii <devskjj@gmail.com>
2026-03-07 13:08:41 +0600

    Git init

---
+++ src/test/java/kg/attractor/exam_9/Exam9ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.attractor.exam_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
